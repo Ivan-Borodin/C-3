4b0f12cd38c15f63bbd502e74c23171dd3f41ef5
Ivan <ivan241578@gmail.com>
2022-05-23 17:36:33 +0300

    Task 19

---
+++ Task19/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int n=a;
+int m=0;
+while (n>0)
+{
+    int i=n%10;
+    n=n/10;
+    m=m*10+i;
+}
+if (a==m) Console.WriteLine("да");
+else Console.WriteLine("нет");
